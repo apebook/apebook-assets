d6d83f6714a3a4a517f7c52092de8a61b67fc4d5
jianping.xwh <jianping.xwh@taobao.com>
2015-07-19 20:43:08 +0800

    公开书籍

--- src/dash/update-book.js
+++ src/dash/update-book.js
@@ -9,14 +9,37 @@ module.exports = function(){
     //点击更新书籍按钮
     $btn.on('click',function(){
         if(update) return false;
-        notify.notify({boxCls:'warning',mPartMsg: "更新中请稍等"});
+        notify.notify({boxCls:'warning',mPartMsg: "更新中请稍等五分钟"});
         $btn.text('更新中...');
         update = true;
         io.post('/api/book/sync',{id:$('.J_BookId').val()}).then(function(result){
             var data = result[0];
-            notify.notify({boxCls:'success',mPartMsg: data.msg});
+            if(!data.success){
+                notify.notify({boxCls:'warn',mPartMsg: data.msg});
+            }else{
+                notify.notify({boxCls:'success',mPartMsg: data.msg||'更新成功'});
+            }
             update = false;
             $btn.text('更新书籍');
+        })
+    });
+
+    //公开书籍
+    var $publishBtn = $('.J_OpenBook');
+    $publishBtn.on('click',function(){
+        var status = $publishBtn.attr('data-status');
+        io.post('/api/book/post',{id:$('.J_BookId').val(),openStatus:status}).then(function(result){
+            debugger;
+            var data = result[0];
+            if(!data.success){
+                notify.notify({boxCls:'warn',mPartMsg: data.msg});
+                return false;
+            }else{
+                notify.notify({boxCls:'success',mPartMsg: '状态设置成功'});
+            }
+            $publishBtn.text(status === 'open' && '隐藏书籍' || '公开书籍');
+            $('.J_OpenStatus').text(status === 'open' && '隐藏书籍' || '公开书籍');
+            $publishBtn.attr(status === 'open' && 'hide' || 'open');
         })
     })
 };
