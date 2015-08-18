041fd656e1e702e567649e7a7fc00773b00a91a5
jianping.xwh <jianping.xwh@taobao.com>
2015-08-18 22:47:36 +0800

    优化表单样式

---
+++ src/book/create-form.js
@@ -0,0 +1,23 @@
+
+var Auth = require('kg/auth/2.0.9/');
+var Msg = require('kg/auth/2.0.9/plugin/msgs/');
+require('kg/auth/2.0.9/plugin/msgs/style.css');
+var io = require('io');
+module.exports = function(){
+    var auth = new Auth('#J_Auth');
+    auth.plug(new Msg());
+    auth.register('exist', function (value,attr,defer,field) {
+        var self = this;
+        io.getJSON("/api/book/exist",{uri:value}).then(function(result){
+            var data = result[0];
+            if(data.exist){
+                defer.reject(self);
+            }else{
+                defer.resolve(self);
+
+            }
+        });
+        return defer.promise;
+    });
+    auth.render();
+};
