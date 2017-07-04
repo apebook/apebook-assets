e14d75990e47cd03bb7fb4c0e988528b2c41dc73
jianping.xwh <jianping.xwh@taobao.com>
2017-07-04 20:45:02 +0800

    优化超时

--- build/deps.js
+++ build/deps.js
@@ -50,10 +50,6 @@ KISSY.config('requires',{
         "kg/auth/2.0.9/plugin/msgs/style.css",
         "io"
     ],
-    "apebook/common/login": [
-        "node",
-        "kg/modal/1.5.2/index"
-    ],
     "apebook/bundle/dropdown": [
         "jquery"
     ],
@@ -108,6 +104,10 @@ KISSY.config('requires',{
         "jquery",
         "lodash",
         "./events"
+    ],
+    "apebook/common/login": [
+        "node",
+        "kg/modal/1.5.2/index"
     ],
     "apebook/dash/cover": [
         "node"
--- src/dash/update-book.js
+++ src/dash/update-book.js
@@ -1,46 +1,53 @@
 //更新书籍
 var $ = require('node').all;
-var io = require('io');
+var IO = require('io');
 var Notify = require('kg/notification/1.0.0/');
-module.exports = function(){
-    var notify = new Notify();
-    var $btn = $('.J_UpdateBook');
-    var update = false;
-    //点击更新书籍按钮
-    $btn.on('click',function(){
-        if(update) return false;
-        notify.notify({boxCls:'warning',mPartMsg: "更新中请稍等五分钟"});
-        $btn.text('更新图书中...');
-        update = true;
-        io.post('/api/book/sync',{id:$('.J_BookId').val()}).then(function(result){
-            var data = result[0];
-            if(!data.success){
-                notify.notify({boxCls:'warn',mPartMsg: data.msg});
-            }else{
-                notify.notify({boxCls:'success',mPartMsg: data.msg||'更新成功'});
-            }
-            update = false;
-            setTimeout(function(){
-              location.reload();
-            },1000);
-        })
+module.exports = function() {
+  var notify = new Notify();
+  var $btn = $('.J_UpdateBook');
+  var update = false;
+  //点击更新书籍按钮
+  $btn.on('click', function() {
+    if (update) return false;
+    notify.notify({boxCls: 'warning', mPartMsg: "更新中请稍等五分钟"});
+    $btn.text('更新图书中...');
+    update = true;
+    new IO({
+      type: "post",
+      url: '/api/book/sync',
+      timeout: 6000,
+      data: {id: $('.J_BookId').val()},
+      success: function(result) {
+        var data = result[0];
+        if (!data.success) {
+          notify.notify({boxCls: 'warn', mPartMsg: data.msg});
+        } else {
+          notify.notify({boxCls: 'success', mPartMsg: data.msg || '更新成功'});
+        }
+        update = false;
+        setTimeout(function() {
+          location.reload();
+        }, 1000);
+      },
+      dataType: "json"
     });
+  });
 
-    //公开书籍
-    var $publishBtn = $('.J_OpenBook');
-    $publishBtn.on('click',function(){
-        var status = $publishBtn.attr('data-status');
-        io.post('/api/book/post',{id:$('.J_BookId').val(),openStatus:status}).then(function(result){
-            var data = result[0];
-            if(!data.success){
-                notify.notify({boxCls:'warn',mPartMsg: data.msg});
-                return false;
-            }else{
-                notify.notify({boxCls:'success',mPartMsg: '状态设置成功'});
-            }
-            $publishBtn.text(status === 'open' && '隐藏图书' || '公开图书');
-            $('.J_OpenStatus').text(status === 'open' && '隐藏图书' || '公开图书');
-            $publishBtn.attr(status === 'open' && 'hide' || 'open');
-        })
+  //公开书籍
+  var $publishBtn = $('.J_OpenBook');
+  $publishBtn.on('click', function() {
+    var status = $publishBtn.attr('data-status');
+    io.post('/api/book/post', {id: $('.J_BookId').val(), openStatus: status}).then(function(result) {
+      var data = result[0];
+      if (!data.success) {
+        notify.notify({boxCls: 'warn', mPartMsg: data.msg});
+        return false;
+      } else {
+        notify.notify({boxCls: 'success', mPartMsg: '状态设置成功'});
+      }
+      $publishBtn.text(status === 'open' && '隐藏图书' || '公开图书');
+      $('.J_OpenStatus').text(status === 'open' && '隐藏图书' || '公开图书');
+      $publishBtn.attr(status === 'open' && 'hide' || 'open');
     })
+  })
 };
